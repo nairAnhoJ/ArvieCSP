62eb9701da4d2f8aabdd75eeeb44857d18ab538b
John Arian <johnarian@pop-os.localdomain>
2022-09-13 23:22:25 +0800

    Landing Page - Navbar

---
+++ header.php
@@ -0,0 +1,33 @@
+<nav class="navbar px-2 sm:px-4 py-2.5 sm:pt-2.5 bg-emerald-500 fixed w-full z-20 top-0 left-0 border-b border-gray-600">
+  <div class="container flex flex-wrap justify-between items-center mx-auto">
+
+    <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 text-sm text-gray-900 rounded-lg md:hidden focus:outline-none" aria-controls="navbar-sticky" aria-expanded="false">
+            <span class="sr-only">Open main menu</span>
+            <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+    </button>
+
+    <a href="./index.php" class="flex items-center">
+        <img src="./images/logo.png" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo">
+        <span class="self-center text-l font-semibold whitespace-nowrap text-gray-900">ARVIE CSP</span>
+    </a>
+    <div class="flex md:order-2">
+        <button type="button" class="text-gray-900 hover:text-white border border-gray-900 hover:border-white font-medium rounded-lg text-sm px-7 py-2.5 text-center mr-3 md:mr-0">Login</button>
+    </div>
+    <div class="nav-items hidden justify-between items-center w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+        <ul class="flex flex-col md:py-4 pt-6 pb-3 mt-3 bg-emerald-500 border-t border-gray-800 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0">
+        <li>
+            <a href="#" class="block py-2 pr-4 pl-3 text-gray-900 bg-emerald-400 rounded md:bg-transparent md:text-gray-900 md:p-0" aria-current="page">Home</a>
+        </li>
+        <li>
+            <a href="#" class="block py-2 pr-4 pl-3 text-gray-100 hover:text-gray-900 rounded hover:bg-emerald-400 md:hover:bg-transparent md:hover:text-blue-700 md:p-0">About</a>
+        </li>
+        <li>
+            <a href="#" class="block py-2 pr-4 pl-3 text-gray-100 hover:text-gray-900 rounded hover:bg-emerald-400 md:hover:bg-transparent md:hover:text-blue-700 md:p-0">Services</a>
+        </li>
+        <li>
+            <a href="#" class="block py-2 pr-4 pl-3 text-gray-100 hover:text-gray-900 rounded hover:bg-emerald-400 md:hover:bg-transparent md:hover:text-blue-700 md:p-0">Contact</a>
+        </li>
+        </ul>
+    </div>
+  </div>
+</nav>
