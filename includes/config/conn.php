ebd725eb062d00a6fff78b3ad3e353c3e11c8ea7
marerokevin <marerokevin@github.com>
2022-10-09 00:20:39 +0800

    DB fix for testing

--- includes/config/conn.php
+++ includes/config/conn.php
@@ -1,14 +1,14 @@
 <?php
     $server = "localhost";
     $username = "root";
+    $usernamex = "arviecsp";
     $password = "";
+    $passwordx = "p@55w0rd";
     $database = "arviedsproject";
+    $databaseX = "ArvieDS";
 
     if(!$conn = mysqli_connect($server, $username, $password, $database)){
+    } elseif ($conn = mysqli_connect($server, $usernamex, $passwordx, $databasex)) {
         die("Failed to Connect to Database!");
     }
-
-    // if(!$conn = mysqli_connect($server, $username, $password, $database)){
-    //     die("Failed to Connect to Database!");
-    // } else $conn = mysqli_connect($server, $username, $password, $database);
 ?>
