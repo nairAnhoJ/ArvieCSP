565863775cba74de3bd5682f5d7a43d15b7ef580
marerokevin <marerokevin@github.com>
2022-10-11 00:50:28 +0800

    [Database] Deleted users, test for reg.

---
+++ includes/config/ArvieDS.sql
@@ -0,0 +1,189 @@
+-- MariaDB dump 10.19  Distrib 10.7.4-MariaDB, for Linux (x86_64)
+--
+-- Host: localhost    Database: ArvieDS
+-- ------------------------------------------------------
+-- Server version	10.7.4-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `accounts`
+--
+
+DROP TABLE IF EXISTS `accounts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `accounts` (
+  `id` int(60) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `email_address` varchar(255) NOT NULL,
+  `pass` varchar(255) NOT NULL,
+  `contact_number` int(15) NOT NULL,
+  `date` datetime NOT NULL,
+  `access` varchar(8) NOT NULL,
+  `permission` varchar(8) NOT NULL,
+  `referralId` varchar(16) NOT NULL,
+  `homeaddress` varchar(255) NOT NULL,
+  `tin_acct` varchar(255) NOT NULL,
+  `sss_num` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `accounts`
+--
+
+LOCK TABLES `accounts` WRITE;
+/*!40000 ALTER TABLE `accounts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `accounts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `invites`
+--
+
+DROP TABLE IF EXISTS `invites`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `invites` (
+  `invitesID` int(30) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  `idOfInvite` int(30) NOT NULL,
+  `invitee` varchar(20) NOT NULL,
+  `inviteeID` int(30) NOT NULL,
+  PRIMARY KEY (`invitesID`)
+) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `invites`
+--
+
+LOCK TABLES `invites` WRITE;
+/*!40000 ALTER TABLE `invites` DISABLE KEYS */;
+INSERT INTO `invites` VALUES
+(32,'James Orozo',13,'Kevin Roy marero',11),
+(41,'Cedrick James Orozo',12,'James Orozo',13),
+(43,'q e',17,'James Orozo',13);
+/*!40000 ALTER TABLE `invites` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `relationship`
+--
+
+DROP TABLE IF EXISTS `relationship`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `relationship` (
+  `level` int(7) NOT NULL,
+  `dl` int(10) NOT NULL,
+  `rbt` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `relationship`
+--
+
+LOCK TABLES `relationship` WRITE;
+/*!40000 ALTER TABLE `relationship` DISABLE KEYS */;
+INSERT INTO `relationship` VALUES
+(1,10,500),
+(2,100,10),
+(3,1000,10),
+(4,10000,10),
+(5,100000,10),
+(6,500000,10),
+(7,500000,10),
+(8,500000,10),
+(9,500000,10),
+(10,500000,10);
+/*!40000 ALTER TABLE `relationship` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `totalbalance`
+--
+
+DROP TABLE IF EXISTS `totalbalance`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `totalbalance` (
+  `totalBalanceId` int(20) NOT NULL AUTO_INCREMENT,
+  `userID` int(30) NOT NULL,
+  `userName` varchar(100) NOT NULL,
+  `totalBalance` int(20) NOT NULL,
+  PRIMARY KEY (`totalBalanceId`)
+) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `totalbalance`
+--
+
+LOCK TABLES `totalbalance` WRITE;
+/*!40000 ALTER TABLE `totalbalance` DISABLE KEYS */;
+INSERT INTO `totalbalance` VALUES
+(27,13,'cedrickjames.orozo@cvsu.edu.ph',1000),
+(28,11,'marero@gmail.com',520),
+(39,12,'cedrickorozo@gmail.com',0),
+(40,17,'arian@gmail.com',0);
+/*!40000 ALTER TABLE `totalbalance` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `transaction`
+--
+
+DROP TABLE IF EXISTS `transaction`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `transaction` (
+  `transactionId` int(50) NOT NULL AUTO_INCREMENT,
+  `type` varchar(50) NOT NULL,
+  `userName` varchar(50) NOT NULL,
+  `userId` int(50) NOT NULL,
+  `inviteName` varchar(50) NOT NULL,
+  `inviteeName` varchar(50) NOT NULL,
+  `addedAmount` int(50) NOT NULL,
+  `TotalBalance` int(50) NOT NULL,
+  PRIMARY KEY (`transactionId`)
+) ENGINE=InnoDB AUTO_INCREMENT=75 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `transaction`
+--
+
+LOCK TABLES `transaction` WRITE;
+/*!40000 ALTER TABLE `transaction` DISABLE KEYS */;
+INSERT INTO `transaction` VALUES
+(69,'Direct Referral','cedrickjames.orozo@cvsu.edu.ph',13,'Cedrick James Orozo','James Orozo',500,500),
+(70,'Indirect Referral','Kevin Roy marero',11,'Cedrick James Orozo','James Orozo',10,510),
+(73,'Direct Referral','cedrickjames.orozo@cvsu.edu.ph',13,'q e','James Orozo',500,1000),
+(74,'Indirect Referral','Kevin Roy marero',11,'q e','James Orozo',10,520);
+/*!40000 ALTER TABLE `transaction` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-10-11  0:49:41
