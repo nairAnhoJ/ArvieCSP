d4041d698d31812ed5cf130889ae85349c868b8c
marerokevin <marerokevin@github.com>
2022-10-18 21:48:40 +0800

    dafuq happened

--- admin/codes-arian.php
+++ admin/codes-arian.php
@@ -200,7 +200,7 @@ include_once ("../includes/config/conn.php");
                 <div style="transform: translate(-50%, 0); z-index:99;" id="errorIDNum" class="hidden absolute top-24 left-1/2  flex items-center p-4 mb-4 w-full max-w-xs text-gray-500 bg-red-300 rounded-lg shadow" role="alert">
                     <div class="inline-flex flex-shrink-0 justify-center items-center w-8 h-8 text-red-500 bg-red-300 rounded-lg">
                         <svg aria-hidden="true" class="w-8 h-8" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                        <span class="sr-only">Error icon</span>
+                        <span class="sr-only">Error icons</span>
                     </div>
                     <div class="ml-3 font-normal text-lg text-gray-900">Invalid ID Number!</div>
                     <button type="button" class="ml-auto -mx-1.5 -my-1.5 bg-gray-100 text-gray-800 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8" data-dismiss-target="#errorIDNum" aria-label="Close">
